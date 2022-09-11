e71ba99cd8d78257ddd083aceb900153815d82dc
Justin <ju3tin@hotmail.co.uk>
2022-09-11 20:30:18 +0100

    er

---
+++ app/README.md
@@ -0,0 +1,15 @@
+# RASA-Mental-Health-Chatbot
+
+Mental health chatbot developed in RASA Framework, to analyse person's chances of mental illness on the basis of survey questionnaire. Have a fun conversation with the bot and upplift your mood!
+
+# Made using RASA Framework
+
+RASA is an open-source framework used to Build contextual AI assistants and chatbots in text and voice with open source machine learning framework.
+
+# Mental Health Chatbot development and Getting Started with RASA:
+Checkout my blog for instructions from RASA Installation to developing chatbots : [My Medium Blog](https://medium.datadriveninvestor.com/rasa-ai-chatbot-for-mental-health-1b3f13827ce3)
+
+# Demonstration:
+
+<img src= "https://github.com/stutisehgal/RASA-Mental-Health-Chatbot/blob/master/rasa%20op.PNG">
+
--- app/actions/actions.py
+++ app/actions/actions.py
@@ -1,5 +1,6 @@
 # This files contains your custom actions which can be used to run
-# custom Python code.
+# custom Python code. Action is an operation that is performed by the chatbot. It could be replying or giving a response,
+# querying a database, hard code reply or API response.  
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
@@ -7,10 +8,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +26,17 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+class ActionHelloWorld(Action):
+
+     def name(self) -> Text:
+         return "action_hello_world"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         print("I am Rasa built mental health chatbot")
+         dispatcher.utter_message(text="Hello!")
+
+         return []
